d635e357c45b887e50705369bac4990f216fe9a6
prasannapokhrel <159265+prasannapokhrel@users.noreply.github.com>
2026-09-06 08:59:35 +0545

    Add starter Python examples

---
+++ variable.py
@@ -0,0 +1,21 @@
+# variable.py
+# Demonstrates the use of variables in Python
+
+
+# Numbers
+age = 25      # integer
+height = 1.75  # float
+
+# Text
+name = "Sarvagya" # string
+
+# Boolean
+is_student = True
+gender_male = True
+
+
+print("Name", name)
+print("Age", age, "Height:", height)
+print("Is Student?", is_student)
+print("Is Male?", gender_male)
+
