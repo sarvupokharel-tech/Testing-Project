9e3141726e1ff1cf336b8a874a9dc37ff084e91f
prasannapokhrel <159265+prasannapokhrel@users.noreply.github.com>
2026-09-06 08:57:07 +0545

    Add simple hello world program in Python

---
+++ hello.py
@@ -0,0 +1,8 @@
+# hello.py
+# Simple hello world program in Python
+def main ():
+    name = input("Enter your name ")
+    print(f"Hello, {name}! Welcome to programming in Python.")
+
+if __name__ == "__main__":
+    main()
